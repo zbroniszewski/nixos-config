6c09ebf3234568bc96065ccf9bcdf22a4f840ffe
zbroniszewski <zachbroniszewski@gmail.com>
2026-03-04 08:41:32 -0500

    chore: add script to rebuild/run ISO in VM

---
+++ rebuild-iso.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+# Build ISO
+nix build \
+  --show-trace \
+  --extra-experimental-features "nix-command flakes" \
+  .#nixosConfigurations.installer.config.system.build.isoImage
+
+# Destroy previous VMs
+for vm in $(virsh --connect qemu:///system list --all --name | grep '^nixos-iso-'); do
+  virsh --connect qemu:///system destroy "$vm"
+  virsh --connect qemu:///system undefine "$vm" \
+    --remove-all-storage \
+    --nvram
+done
+
+# Install new VM
+new_vm_name="nixos-iso-$(date +"%Y-%m-%d-%H-%M-%S")"
+
+virt-install \
+  --name "$new_vm_name" \
+  --memory 16384 \
+  --vcpus 8 \
+  --disk size=100 \
+  --cdrom $(readlink -f $(find ./result/iso -type f)) \
+  --os-variant nixos-unstable \
+  --graphics spice \
+  --boot uefi,menu=on \
+  --connect qemu:///system \
+  --noreboot \
+  --noautoconsole
+
+virt-manager \
+  --connect qemu:///system \
+  --show-domain-console \
+  "$new_vm_name"
